b29e59d08175bf13bdffbd507b83a2c2528fdf38
Jose Lopez <joseglopez1289@gmail.com>
2021-02-19 06:36:34 -0800

    fixed database connection issues

--- server.js
+++ server.js
@@ -2,26 +2,14 @@
 require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
-
-const passport = require('passport');
-const session = require('express-session');
 const ObjectID = require('mongodb').ObjectID;
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
+
 app.set('view engine', 'pug');
-
-fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.route('/').get((req, res) => {
-  res.render(process.cwd() + '/views/pug/index', 
-  {
-    title: 'Hello', message: 'Please login'
-  });
-});
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
@@ -35,21 +23,21 @@ app.use(passport.session());
 
 myDB(async (client) => {
   const myDataBase = await client.db('database').collection('users');
-
+  
   app.route('/').get((req, res) => {
     res.render('pug', {
       title: 'Connected to Database',
       message: 'Please login'
     });
   });
-
+  
   passport.serializeUser((user, done) => {
     done(null, user._id);
   });
   passport.deserializeUser((id, done) => {
     myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
       done(null, doc);
-   });
+    });
   });
 }).catch((e) => {
   app.route('/').get((req, res) => {
@@ -57,6 +45,17 @@ myDB(async (client) => {
   });
 });
 
+fccTesting(app); //For FCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.route('/').get((req, res) => {
+//   res.render(process.cwd() + '/views/pug/index', 
+//   {
+//     title: 'Hello', message: 'Please login'
+//   });
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
