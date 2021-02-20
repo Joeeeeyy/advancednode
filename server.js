c21c249570a9f3fb5b95eb809a88161895ec6dfb
Jose Lopez <joseglopez1289@gmail.com>
2021-02-20 00:54:58 -0800

    fixed database connection

--- server.js
+++ server.js
@@ -9,13 +9,6 @@ const passport = require('passport');
 
 const app = express();
 
-fccTesting(app); // For fCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
-
 app.set('view engine', 'pug');
 
 app.use(session({
@@ -49,19 +42,15 @@ myDB(async client => {
   });
 }).catch(e => {
   app.route('/').get((req, res) => {
-    const resjson = { title: e, message: 'Unable to login' 
-  };
-  res.render('pug/index', 
-  resjson);
+    res.render('pug', { title: e, message: 'Unable to login' });
   });
 });
 
-// app.route('/').get((req, res) => {
-//   res.render(process.cwd() + '/views/pug/index', {
-//     title: 'Hello',
-//     message: 'Please login'
-//   });
-// });
+fccTesting(app); // For fCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port ' + process.env.PORT);
