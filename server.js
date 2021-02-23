b7550c07e85b5fbff42230cf4ab51ea597754afb
Jose Lopez <joseglopez1289@gmail.com>
2021-02-22 17:59:39 -0800

    challenge #11 Registration of New Users

--- server.js
+++ server.js
@@ -37,25 +37,51 @@ myDB(async client => {
     res.render('pug', {
       title: 'Connected to Database',
       message: 'Please login',
-      showLogin: true
+      showLogin: true,
+      showRegistration: true
     });
   });
-
   app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
     res.redirect('/profile');
   });
-
+  
   app.route('/profile').get(ensureAuthenticated, (req, res) => {
     res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
   });
   
-app.route('/logout')
-    .get((req, res) => {
-      req.logout();
-      res.redirect('/');
-});
-
-app.use((req, res, next) => {
+  app.route('/logout')
+  .get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+  
+    app.route('/register').post((req, res, next) => {
+      myDataBase.findOne({ username: req.body.username }, function(err, user) {
+        if (err) {
+          next (err);
+        } else if (user) {
+          res.redirect('/');
+        } else {
+          myDataBase.insertOne({
+            username: req.body.username,
+            password: req.body.password
+          },
+          (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              next(null, doc.ops[0]);
+            }
+          }
+          )
+        }
+      })
+    },
+    passport.authenticate('local', { failureRedirect: '/' }), (req, res, next) => {
+      res.redirect('/profile');
+    }
+    );
+  app.use((req, res, next) => {
   res.status(404)
       .type('text')
       .send('Not Found');
