5125999698561a4e71e4021fb046509018914a34
Jose Lopez <joseglopez1289@gmail.com>
2021-02-22 19:23:30 -0800

    fixed error so BCrypt is correctly implemented

--- server.js
+++ server.js
@@ -95,11 +95,13 @@ passport.use(new LocalStrategy(
     myDataBase.findOne({ username: username }, function (err, user) {
       if (err) { return done(err); }
       if (!user) { return done(null, false); }
-      if (password !== user.password) { return done(null, false); }
+      if (!bcrypt.compareSync(password, user.password)) { 
+        return done(null, false); 
+      }
       return done(null, user);
-      });
-    }
-  ));
+    });
+  }
+));
 }).catch((e) => {
   app.route('/').get((req, res) => {
     res.render('pug', { title: e, message: 'Unable to login' });
