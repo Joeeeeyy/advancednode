05d7906aa8ab5edd28133e6d44e396a954b4c7e8
Jose Lopez <joseglopez1289@gmail.com>
2021-02-25 08:12:46 -0800

    challenge # 17 Communicating by Emitting

--- server.js
+++ server.js
@@ -39,7 +39,10 @@ myDB(async (client) => {
   routes(app, myDataBase);
   auth(app, myDataBase);
 
+  let currrentUsers = 0;
   io.on('connection', (socket) => {
+    ++currrentUsers;
+    io.emit('user count', currrentUsers);
     console.log('A user has connected');
   });
 }).catch((e) => {
