1e118afca97143428c9e86183134f05c8b70fd4a
Jose Lopez <joseglopez1289@gmail.com>
2021-02-28 23:35:15 -0800

    Announce New Users

--- public/client.js
+++ public/client.js
@@ -2,14 +2,17 @@ $(document).ready(function () {
   /* Global io*/
   let socket = io();
 
-  socket.on('user count', function (data) {
-    console.log(data);
-  });
+socket.on('user', data => {
+  $('#num-users').text(data.currentUsers + ' users online');
+  let message =
+    data.name +
+    (data.connected ? ' has joined the chat.' : ' has left the chat.');
+  $('#messages').append($('<li>').html('<b>' + message + '</b>'));
+});
 
   // Form submittion with new message in field with id 'm'
   $('form').submit(function () {
     let messageToSend = $('#m').val();
-    // Send message to server here?
     $('#m').val('');
     return false; // Prevent form submit from refreshing page
   });
